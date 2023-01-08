6a5d1430b05a47c801a1cc96c3d8f0fe56714154
surajnakhale810 <surajnakhale810@gmail.com>
2023-01-08 14:07:13 +0530

    random user2

--- script.js
+++ script.js
@@ -7,46 +7,46 @@ let btnPhone = document.getElementById("phone")
 let additionalInform = document.getElementById("inform")
 let btnGetUser = document.getElementById("getUser")
 
-let displayAge = 28 
+let displayAge = 28
 let displayEmail = "surasddd@gmail.com"
-let displayPhone = 2457812345 
+let displayPhone = 2457812345
 
-async function myFunction(){
+async function myFunction() {
     let response = await fetch("https://randomuser.me/api/")
     let response1 = await response.json();
-   let user = await response1.results[0]
-//    console.log(user) 
-myWork(user);   
+    let user = await response1.results[0]
+    //    console.log(user) 
+    myWork(user);
 }
 
-function myWork(user){
-btnName.innerText = `${user.name.title} ${user.name.first} ${user.name.last}`
+function myWork(user) {
+    btnName.innerText = `${user.name.title} ${user.name.first} ${user.name.last}`
 
-// btnImage.setAttribute('src',`${user.picture.large}`);   // this also work for random image change.
+    // btnImage.setAttribute('src',`${user.picture.large}`);   // this also work for random image change.
 
-btnImage.src = user.picture.large
+    btnImage.src = user.picture.large
 
-displayAge = user.dob.age
-displayEmail = user.email
-displayPhone = user.phone
+    displayAge = user.dob.age
+    displayEmail = user.email
+    displayPhone = user.phone
 
 }
-function ageFun(){
+function ageFun() {
     additionalInform.innerText = displayAge
 }
-btnAge.addEventListener("click",ageFun)
+btnAge.addEventListener("click", ageFun)
 
-function emailFun(){
+function emailFun() {
     additionalInform.innerText = displayEmail
 }
-btnEmail.addEventListener("click",emailFun)
+btnEmail.addEventListener("click", emailFun)
 
-function phoneFun(){
+function phoneFun() {
     additionalInform.innerText = displayPhone
 }
-btnPhone.addEventListener("click",phoneFun)
+btnPhone.addEventListener("click", phoneFun)
 
 // myFunction()
 
-btnGetUser.addEventListener("click",myFunction)
+btnGetUser.addEventListener("click", myFunction)
 
