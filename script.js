12e12778da55d277c4290298cfbd7df078e1a821
surajnakhale810 <surajnakhale810@gmail.com>
2023-01-11 15:23:16 +0530

    user1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//your code here
+/*
 let btnImage = document.getElementById("image")
 let btnName = document.getElementById("name")
 let btnAge = document.getElementById("age")
@@ -15,8 +15,7 @@ async function myFunction() {
     let response = await fetch("https://randomuser.me/api/")
     let response1 = await response.json();
     let user = await response1.results[0]
-    //    console.log(user) 
-    // myWork(user);
+   
 
 function myWork(user) {
     btnName.innerText = `${user.name.title} ${user.name.first} ${user.name.last}`
@@ -47,7 +46,50 @@ function phoneFun() {
 }
 btnPhone.addEventListener("click", phoneFun)
 
-// myFunction()
 
 btnGetUser.addEventListener("click", myFunction)
 
+*/
+
+
+
+const imageElement = document.getElementById('image');
+const nameElement = document.getElementById('name');
+const ageButton = document.getElementById('age');
+const emailButton = document.getElementById('email');
+const phoneButton = document.getElementById('phone');
+const additionalInfoElement = document.getElementById('additional-info');
+const getUserButton = document.getElementById('getUser');
+
+const RANDOM_USER_API = "https://randomuser.me/api/";
+let age = 24;
+let email = "avi7000201383@gmail.com";
+let phone = "(+91) 7000201383";
+
+function getRandomUser(){
+     return fetch(RANDOM_USER_API).then(res => res.json()).then(res => res.results[0]);
+}
+
+async function renderRandomUser(){
+     let randomUser = await getRandomUser();
+     imageElement.src = randomUser.picture.large;
+     nameElement.innerText = `${randomUser.name.first} ${randomUser.name.last}`;
+     age = randomUser.dob.age;
+     email = randomUser.email;
+     phone = randomUser.phone;
+     console.log(randomUser);
+}
+
+renderRandomUser();
+
+ageButton.addEventListener('click', _ => {
+     additionalInfoElement.innerText = age;
+});
+emailButton.addEventListener('click', _ => {
+     additionalInfoElement.innerText = email;
+});
+phoneButton.addEventListener('click', _ => {
+     additionalInfoElement.innerText = phone;
+});
+
+getUserButton.addEventListener('click', renderRandomUser);
