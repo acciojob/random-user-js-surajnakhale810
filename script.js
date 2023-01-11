6f4e268b91f9fb40a81c66d0185475d205ee5d8b
surajnakhale810 <surajnakhale810@gmail.com>
2023-01-11 16:43:24 +0530

    random1

--- script.js
+++ script.js
@@ -53,43 +53,43 @@ btnGetUser.addEventListener("click", myFunction)
 
 
 
-const imageElement = document.getElementById('image');
-const nameElement = document.getElementById('name');
-const ageButton = document.getElementById('age');
-const emailButton = document.getElementById('email');
-const phoneButton = document.getElementById('phone');
-const additionalInfoElement = document.getElementById('additional-info');
-const getUserButton = document.getElementById('getUser');
-
-const RANDOM_USER_API = "https://randomuser.me/api/";
-let age = 24;
-let email = "avi7000201383@gmail.com";
-let phone = "(+91) 7000201383";
-
-function getRandomUser(){
-     return fetch(RANDOM_USER_API).then(res => res.json()).then(res => res.results[0]);
-}
-
-async function renderRandomUser(){
-     let randomUser = await getRandomUser();
-     imageElement.src = randomUser.picture.large;
-     nameElement.innerText = `${randomUser.name.first} ${randomUser.name.last}`;
-     age = randomUser.dob.age;
-     email = randomUser.email;
-     phone = randomUser.phone;
-     console.log(randomUser);
-}
-
-renderRandomUser();
-
-ageButton.addEventListener('click', _ => {
-     additionalInfoElement.innerText = age;
-});
-emailButton.addEventListener('click', _ => {
-     additionalInfoElement.innerText = email;
-});
-phoneButton.addEventListener('click', _ => {
-     additionalInfoElement.innerText = phone;
-});
-
-getUserButton.addEventListener('click', renderRandomUser);
+async function fetchdata() {
+    const url = "https://randomuser.me/api/";
+    const data = await fetch(url);
+    const response = await data.json();
+    // console.log(response);
+    return response.results[0];
+  }
+  const showuser = () => {
+    fetchdata().then((data) => {
+      console.log(data.name.first, data.name.last);
+      console.log(data.picture.large);
+      document.getElementById(
+        "img-box"
+      ).innerHTML = `<img src="${data.picture.large}">`;
+      document.getElementById(
+        "username"
+      ).innerHTML = `${data.name.first} ${data.name.last}`;
+      const ageButton = document.getElementById("age");
+      ageButton.addEventListener("click", () => {
+        console.log(data.dob.age);
+        document.getElementById("output").innerHTML = `<h1>${data.dob.age}<h1>`;
+      });
+      const EmailButton = document.getElementById("email");
+      EmailButton.addEventListener("click", () => {
+        console.log(data.email);
+        document.getElementById("output").innerHTML = `<h1>${data.email}<h1>`;
+      });
+      const PhoneButton = document.getElementById("phone");
+      PhoneButton.addEventListener("click", () => {
+        console.log(data.phone);
+        document.getElementById("output").innerHTML = `<h1>${data.phone}<h1>`;
+      });
+    });
+  };
+  
+  showuser();
+  const newUser =document.getElementById("getUser");
+  newUser.addEventListener("click", () => {
+    showuser();
+  });
